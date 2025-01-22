65c708837d75bd1b4ff239b0f097caca74633c80
Otis Murphy <murphyot@shernet.sheridancollege.ca>
2025-01-22 11:02:36 -0500

    car moves dow the screen and loops on edge. should be to apply that code to each car

--- Assets/scripts/LERPcarMovement.cs
+++ Assets/scripts/LERPcarMovement.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class LERPcarMovement : MonoBehaviour
 {
-
+    public float t;
     public Vector2 start;
+    public Vector2 end;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +18,11 @@ public class LERPcarMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        t += 1 * Time.deltaTime;
+        if (transform.position.y <= -6) 
+        {
+            t = 0;
+        }
+        transform.position = Vector2.Lerp(start, end, t);
     }
 }
